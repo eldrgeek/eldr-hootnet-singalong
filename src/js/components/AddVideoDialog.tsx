d092d45576cc811bcf01223a3e47c62de4bee168
Mike Wolf <mw@mike-wolf.com>
2020-10-09 18:34:12 -0400

    fix button location and use Tailwind border

--- src/js/components/AddVideoDialog.tsx
+++ src/js/components/AddVideoDialog.tsx
@@ -1,119 +1,119 @@
 //@ts-ignore
-import React from "react"; //eslint-disable-line
+import React from 'react'; //eslint-disable-line
 //@ts-ignore
-import { CurrentModule, useApp, register } from "../util/CurrentModule"; //eslint-disable-line
+import { CurrentModule, useApp, register } from '../util/CurrentModule'; //eslint-disable-line
 // import { IconButton } from '@material-ui/core';
-import ButtonBase from "./ButtonBase";
-import { faCheck, faWindowClose } from "@fortawesome/free-solid-svg-icons";
-import { Input } from "@material-ui/core";
+import ButtonBase from './ButtonBase';
+import { faCheck, faWindowClose } from '@fortawesome/free-solid-svg-icons';
+import { Input } from '@material-ui/core';
 //@ts-ignore
 const CL = (...args) => {
-  console.log(...args, `(/src/js/util/CreateModules.tsx)`);
+	console.log(...args, `(/src/js/util/CreateModules.tsx)`);
 };
 
 const Component = () => {
-  const { actions, state } = useApp();
-  type Mode = {
-    title: string;
-    onClick: any;
-  };
-  const modeNull: Mode = {
-    title: "change title",
-    onClick: () => console.log("change")
-  };
-  const [mode, setMode] = React.useState(modeNull);
-  React.useEffect(() => {
-    switch (state.UI.dialogType) {
-      case "loadconfig":
-        setMode({
-          title: "Load singalong configuration",
-          onClick: () => {
-            console.log("Load congig");
-          }
-        });
-        break;
-      case "saveconfig":
-        setMode({
-          title: "Save configuration as",
-          onClick: () => {
-            console.log("Save congig");
-          }
-        });
-        break;
-      case "add":
-        setMode({
-          title: "Enter video URL",
-          onClick: () => {
-            CL("select the video");
-            //@ts-ignore
-            actions.videos.add(state.videos.videoTitle);
-            //@ts-ignore
-            actions.UI.setDialogType("");
-          }
-        });
-        break;
-      case "save":
-        setMode({
-          title: "Save file as...",
-          onClick: () => {
-            console.log("Save congig");
-          }
-        });
-        break;
-      case "load":
-        break;
-      default:
-        break;
-    }
-  }, [state.UI.dialogType]); //eslint-disable-line
+	const { actions, state } = useApp();
+	type Mode = {
+		title: string;
+		onClick: any;
+	};
+	const modeNull: Mode = {
+		title: 'change title',
+		onClick: () => console.log('change')
+	};
+	const [mode, setMode] = React.useState(modeNull);
+	React.useEffect(() => {
+		switch (state.UI.dialogType) {
+			case 'loadconfig':
+				setMode({
+					title: 'Load singalong configuration',
+					onClick: () => {
+						console.log('Load congig');
+					}
+				});
+				break;
+			case 'saveconfig':
+				setMode({
+					title: 'Save configuration as',
+					onClick: () => {
+						console.log('Save congig');
+					}
+				});
+				break;
+			case 'add':
+				setMode({
+					title: 'Enter video URL',
+					onClick: () => {
+						CL('select the video');
+						//@ts-ignore
+						actions.videos.add(state.videos.videoTitle);
+						//@ts-ignore
+						actions.UI.setDialogType('');
+					}
+				});
+				break;
+			case 'save':
+				setMode({
+					title: 'Save file as...',
+					onClick: () => {
+						console.log('Save congig');
+					}
+				});
+				break;
+			case 'load':
+				break;
+			default:
+				break;
+		}
+	}, [state.UI.dialogType]); //eslint-disable-line
 
-  //@ts-ignore
-  const setValue = (e) => {
-    //eslint-disable-line
-    //eslint-disable-line
-    e.persist();
-    CL("setting value", e.target.value);
-    //@ts-ignore
-    actions.videos.setVideoTitle(e.target.value);
-    // CL('setting', e.target.value);
-  };
-  if (state.UI.dialogType === "") return null;
-  return (
-    <React.Fragment>
-      <div
-        style={{ border: "1px solid black", padding: "6px", width: "500px" }}
-      >
-        <div>{mode.title}</div>
-        <div>
-          <Input
-            style={{ margin: "2px 23px 2px 10px", width: "350px" }}
-            id="input-add"
-            //@ts-ignore
-            onChange={(e) => actions.videos.setVideoTitle(e.target.value)}
-            value={state.videos.videoTitle}
-            placeholder="Enter video URL"
-          />
-        </div>
-        <div>
-          <ButtonBase
-            id="button-confirm"
-            disabled={false}
-            onClick={mode.onClick}
-            buttoncolor="blue"
-            icon={faCheck}
-          />
-          <ButtonBase
-            id="button-cancel"
-            disabled={false}
-            //@ts-ignore
-            onClick={() => actions.UI.setDialogType("")}
-            buttoncolor="blue"
-            icon={faWindowClose}
-          />
-        </div>
-      </div>
-    </React.Fragment>
-  );
+	//@ts-ignore
+	const setValue = (e) => {
+		//eslint-disable-line
+		//eslint-disable-line
+		e.persist();
+		CL('setting value', e.target.value);
+		//@ts-ignore
+		actions.videos.setVideoTitle(e.target.value);
+		// CL('setting', e.target.value);
+	};
+	if (state.UI.dialogType === '') return null;
+	return (
+		<React.Fragment>
+			<div className="border-2 border-black">
+				<div>
+					<div className="flex justify-center">{mode.title}</div>
+					<div className="flex justify-center">
+						<Input
+							style={{ margin: '2px 23px 2px 10px', width: '350px' }}
+							id="input-add"
+							//@ts-ignore
+							onChange={(e) => actions.videos.setVideoTitle(e.target.value)}
+							value={state.videos.videoTitle}
+							placeholder="Enter video URL"
+						/>
+					</div>
+				</div>
+				<div className="flex justify-around">
+					<ButtonBase
+						id="button-confirm"
+						disabled={false}
+						onClick={mode.onClick}
+						buttoncolor="blue"
+						icon={faCheck}
+					/>
+					<ButtonBase
+						id="button-cancel"
+						disabled={false}
+						//@ts-ignore
+						onClick={() => actions.UI.setDialogType('')}
+						buttoncolor="blue"
+						icon={faWindowClose}
+					/>
+				</div>
+			</div>
+		</React.Fragment>
+	);
 };
 
 export default Component;
--- src/js/components/VideoMonitor.tsx
+++ src/js/components/VideoMonitor.tsx
@@ -1,51 +1,51 @@
-import React from "react"; //eslint-disable-line
-import { CurrentModule, useApp, register } from "../util/CurrentModule"; //eslint-disable-line
-import ReactPlayer from "react-player";
-import getCameraStream from "../lib/getCameraStream";
-import Blobber from "../lib/blobber";
+import React from 'react'; //eslint-disable-line
+import { CurrentModule, useApp, register } from '../util/CurrentModule'; //eslint-disable-line
+import ReactPlayer from 'react-player';
+import getCameraStream from '../lib/getCameraStream';
+import Blobber from '../lib/blobber';
 //eslint-disable-next-line
 const CL = (...args) => {
-  console.log(...args, __filename);
+	console.log(...args, __filename);
 };
 const Monitor = () => {
-  const [blobber, setBlobber] = React.useState(null);
-  const { state, actions } = useApp();
-  const [cameraStream, setCameraStream] = React.useState(null);
+	const [blobber, setBlobber] = React.useState(null);
+	const { state, actions } = useApp();
+	const [cameraStream, setCameraStream] = React.useState(null);
 
-  React.useEffect(() => {
-    if (state.videos.cameraOn && cameraStream === null) {
-      getCameraStream().then((stream) => {
-        setCameraStream(stream);
-        const b = new Blobber(stream);
-        setBlobber(b);
-      });
-    } else if (!state.videos.cameraOn && cameraStream !== null) {
-      cameraStream.getTracks().forEach((track) => track.stop());
-      setCameraStream(null);
-    }
-  }, [state.videos.cameraOn]); //eslint-disable-line
+	React.useEffect(() => {
+		if (state.videos.cameraOn && cameraStream === null) {
+			getCameraStream().then((stream) => {
+				setCameraStream(stream);
+				const b = new Blobber(stream);
+				setBlobber(b);
+			});
+		} else if (!state.videos.cameraOn && cameraStream !== null) {
+			cameraStream.getTracks().forEach((track) => track.stop());
+			setCameraStream(null);
+		}
+	}, [state.videos.cameraOn]); //eslint-disable-line
 
-  React.useEffect(() => {
-    if (state.videos.recording) {
-      blobber.start(2000);
-    } else if (blobber) {
-      blobber.stopBlobber();
-      //@ts-ignore
-      actions.videos.add(URL.createObjectURL(new Blob(blobber.blobs)));
-      setBlobber(null);
-    }
-  }, [state.videos.recording]); //eslint-disable-line
-  return (
-    <React.Fragment>
-      <ReactPlayer
-        height={"50%"}
-        width={"50%"}
-        muted={true}
-        url={cameraStream}
-        playing={true}
-      />
-    </React.Fragment>
-  );
+	React.useEffect(() => {
+		if (state.videos.recording) {
+			blobber.start();
+		} else if (blobber) {
+			blobber.stopBlobber();
+			//@ts-ignore
+			actions.videos.add(URL.createObjectURL(new Blob(blobber.blobs)));
+			setBlobber(null);
+		}
+	}, [state.videos.recording]); //eslint-disable-line
+	return (
+		<React.Fragment>
+			<ReactPlayer
+				height={'50%'}
+				width={'50%'}
+				muted={true}
+				url={cameraStream}
+				playing={true}
+			/>
+		</React.Fragment>
+	);
 };
 export default Monitor;
 // CurrentModule(Monitor);
