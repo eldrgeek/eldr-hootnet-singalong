28a1925fc09d5546be510494e8f59c758c117413
Mike Wolf <mw@mike-wolf.com>
2020-10-09 15:53:00 -0400

    Changed title icon and heading

--- src/js/components/App.tsx
+++ src/js/components/App.tsx
@@ -1,49 +1,49 @@
-import React from "react"; //eslint-disable-line
-import { CurrentModule, useApp } from "../util/CurrentModule"; //eslint-disable-line
+import React from 'react'; //eslint-disable-line
+import { CurrentModule, useApp } from '../util/CurrentModule'; //eslint-disable-line
 // import Messages from "./Messages";
-import Videos from "./Videos";
-import ButtonCamera from "./ButtonCamera";
-import ButtonPlayPause from "./ButtonPlayPause";
-import ButtonRecordStop from "./ButtonRecordStop";
-import ButtonRewind from "./ButtonRewind";
-import VideoMonitor from "./VideoMonitor";
-import ButtonAddVideo from "./ButtonAddVideo";
-import AddVideoDialog from "./AddVideoDialog";
-import ButtonUpload from "./ButtonUpload";
-import ButtonDownload from "./ButtonDownload";
-import ButtonJoyride from "./ButtonJoyride";
-
+import Videos from './Videos';
+import ButtonCamera from './ButtonCamera';
+import ButtonPlayPause from './ButtonPlayPause';
+import ButtonRecordStop from './ButtonRecordStop';
+import ButtonRewind from './ButtonRewind';
+import VideoMonitor from './VideoMonitor';
+import ButtonAddVideo from './ButtonAddVideo';
+import AddVideoDialog from './AddVideoDialog';
+import ButtonUpload from './ButtonUpload';
+import ButtonDownload from './ButtonDownload';
+import ButtonJoyride from './ButtonJoyride';
+import { H4 } from './Typography';
 // import AppJoyride from "./AppJoyride";
 //@ts-ignore
 const CL = (...args) => {
-  //eslint-disable-line
-  console.log(...args, `(${__filename})`);
+	//eslint-disable-line
+	console.log(...args, `(${__filename})`);
 };
 const App = () => {
-  // const { state, actions } = useApp();
+	// const { state, actions } = useApp();
 
-  return (
-    <React.Fragment>
-      <div>
-        <h1 style={{ display: "flex", justifyContent: "center" }}>
-          HootNet Singalong
-        </h1>
-        <div>
-          <ButtonAddVideo />
-          <ButtonCamera />
-          <ButtonPlayPause />
-          <ButtonRecordStop />
-          <ButtonRewind />
-          <ButtonUpload />
-          <ButtonDownload />
-          <ButtonJoyride />
-        </div>
-        <AddVideoDialog />
-        <VideoMonitor />
-        <br />
-        <Videos />
-        <br />
-        {/* <Videos>
+	return (
+		<React.Fragment>
+			<div>
+				<H4 style={{ display: 'flex', justifyContent: 'center' }}>
+					HootNet SingAlong
+				</H4>
+				<div>
+					<ButtonAddVideo />
+					<ButtonCamera />
+					<ButtonPlayPause />
+					<ButtonRecordStop />
+					<ButtonRewind />
+					<ButtonUpload />
+					<ButtonDownload />
+					<ButtonJoyride />
+				</div>
+				<AddVideoDialog />
+				<VideoMonitor />
+				<br />
+				<Videos />
+				<br />
+				{/* <Videos>
       Videos
       Video
         ReactPlayer
@@ -59,10 +59,10 @@ const App = () => {
       Button: RECORD
       Button: REWiND
       Button: CAMERA" */}
-        {/* <Messages /> */}
-      </div>
-    </React.Fragment>
-  );
+				{/* <Messages /> */}
+			</div>
+		</React.Fragment>
+	);
 };
 export default App;
 // CurrentModule(App);
--- src/js/components/Typography.tsx
+++ src/js/components/Typography.tsx
@@ -1,35 +1,47 @@
-import React from "react";
-import Typography from "@material-ui/core/Typography";
-import { CurrentModule } from "../util/CurrentModule";
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { CurrentModule } from '../util/CurrentModule';
 
 export const H1 = (props) => {
-  return (
-    <Typography align="center" variant="h1" {...props}>
-      {props.children}
-    </Typography>
-  );
+	return (
+		<Typography align="center" variant="h1" {...props}>
+			{props.children}
+		</Typography>
+	);
 };
 
 export const H2 = (props) => {
-  return (
-    <Typography align="center" variant="h2" {...props}>
-      {props.children}
-    </Typography>
-  );
+	return (
+		<Typography align="center" variant="h2" {...props}>
+			{props.children}
+		</Typography>
+	);
 };
 export const H3 = (props) => {
-  React.useEffect(() => {
-    const tailwind = props.tailwind;
-    if (tailwind) {
-    }
-  });
-  return (
-    <Typography align="center" variant="h3" {...props}>
-      {props.children}
-    </Typography>
-  );
+	React.useEffect(() => {
+		const tailwind = props.tailwind;
+		if (tailwind) {
+		}
+	});
+	return (
+		<Typography align="center" variant="h3" {...props}>
+			{props.children}
+		</Typography>
+	);
+};
+export const H4 = (props) => {
+	React.useEffect(() => {
+		const tailwind = props.tailwind;
+		if (tailwind) {
+		}
+	});
+	return (
+		<Typography align="center" variant="h4" {...props}>
+			{props.children}
+		</Typography>
+	);
 };
 function Test() {
-  return <H1>Typography </H1>;
+	return <H1>Typography </H1>;
 }
 CurrentModule(Test);
